279951abf6aed48220cd873bf573873fbb4562f8
Catherine Dean <catherinemdean15@gmail.com>
2021-01-16 19:11:44 -0700

    add bulk discounts controller with index and show action

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,7 +1,28 @@
 class Merchant::BulkDiscountsController < ApplicationController
+  before_action :find_merchant
+  before_action :find_bulk_discount, only: [:show]
 
   def index
+    @bulk_discounts = @merchant.bulk_discounts
+  end
 
+  def destroy
+    BulkDiscount.destroy(params[:id])
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def find_bulk_discount
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
 
 end
